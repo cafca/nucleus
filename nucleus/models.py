70bc29552e42d26871aace45455e360be446418c
Vincent Ahrend <nichte@gmail.com>
2015-05-11 17:20:53 +0200

    Add Identity.color

--- nucleus/models.py
+++ nucleus/models.py
@@ -265,6 +265,7 @@ class Identity(Serializable, db.Model):
     crypt_public = db.Column(db.Text)
     sign_private = db.Column(db.Text)
     sign_public = db.Column(db.Text)
+    color = db.Column(db.String(6), default="C81D25")
 
     vesicles = db.relationship(
         'Vesicle',
